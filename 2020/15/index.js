e242b3f7982f321424a471a5bd5e1ebab2b2d7ed
Tom Cant <tomcant@gmail.com>
2020-12-18 00:06:01 +0000

    Added 2020/day17

--- 2020/15/index.js
+++ 2020/15/index.js
@@ -1,19 +1,12 @@
 const { readRaw } = require('../../utils/file-io');
 
 const findNthTerm = (numbers, n) => {
-  const seen = new Map(
-    numbers.flatMap((number, index) =>
-      index < numbers.length - 1
-        ? [[number, index]]
-        : []
-    )
-  );
+  let last = numbers.pop();
+  const seen = new Map(numbers.map((n, i) => [n, i + 1]));
 
-  let last = numbers[numbers.length - 1];
-
-  for (let i = numbers.length; i < n; ++i) {
-    const term = seen.has(last) ? i - seen.get(last) - 1 : 0;
-    seen.set(last, i - 1);
+  for (let i = numbers.length + 1; i < n; ++i) {
+    const term = seen.has(last) ? i - seen.get(last) : 0;
+    seen.set(last, i);
     last = term;
   }
 
@@ -25,5 +18,4 @@ const part2 = numbers => findNthTerm(numbers, 30000000);
 
 const numbers = readRaw('input.txt').split(',').map(Number);
 
-console.log(part1(numbers));
-console.log(part2(numbers));
+console.log(part1([...numbers]), part2([...numbers]));
