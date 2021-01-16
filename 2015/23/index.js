86315f8456df845e2290edce05c65c08974442b6
Tom Cant <tomcant@gmail.com>
2021-01-16 11:07:48 +0000

    Added 2015/23

---
+++ 2015/23/index.js
@@ -0,0 +1,50 @@
+const { readLines } = require( '../../utils/file-io');
+
+const run = (instructions, initialRegisters) => {
+  let registers = initialRegisters;
+  let next = 0;
+
+  while (next < instructions.length) {
+    const [op, args] = instructions[next];
+    let increment = 1;
+
+    switch (op) {
+      case 'hlf':
+        registers[args[0]] /= 2;
+        break;
+      case 'tpl':
+        registers[args[0]] *= 3;
+        break;
+      case 'inc':
+        registers[args[0]] += 1;
+        break;
+      case 'jmp':
+        increment = +args[0];
+        break;
+      case 'jie':
+        if (registers[args[0]] % 2 === 0) {
+          increment = +args[1];
+        }
+        break;
+      case 'jio':
+        if (registers[args[0]] === 1) {
+          increment = +args[1];
+        }
+        break;
+    }
+
+    next += increment;
+  }
+
+  return registers;
+};
+
+const part1 = instructions => run(instructions, { a: 0, b: 0 }).b;
+const part2 = instructions => run(instructions, { a: 1, b: 0 }).b;
+
+const instructions = readLines('input.txt').map(line => {
+  const [, op, args] = line.match(/([^\s]+)\s(.+)/);
+  return [op, args.split(', ')];
+});
+
+console.log(part1(instructions), part2(instructions));
