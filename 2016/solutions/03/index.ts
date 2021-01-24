82451b578ed18ba906c707c27446ef2d8eaa61d9
Tom Cant <tomcant@gmail.com>
2021-01-24 10:20:00 +0000

    Added 2016/03

---
+++ 2016/solutions/03/index.ts
@@ -0,0 +1,28 @@
+type Triangle = [number, number, number];
+
+const isValid = ([l1, l2, l3]: Triangle): boolean => l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1;
+
+export const part1 = (triangles: Triangle[]): number =>
+  triangles.reduce((validCount, triangle) => validCount + +isValid(triangle), 0);
+
+export const part2 = (triangles: Triangle[]): number => {
+  let validCount = 0;
+
+  for (let i = 0; i < triangles.length; i += 3) {
+    for (let j = 0; j < 3; ++j) {
+      validCount += +isValid([
+        triangles[i][j],
+        triangles[i + 1][j],
+        triangles[i + 2][j]]
+      );
+    }
+  }
+
+  return validCount;
+};
+
+export const parseInput = (input: string): Triangle[] =>
+  input.split('\n').map(line => {
+    const [l1, l2, l3] = line.trim().split(/\s+/);
+    return [+l1, +l2, +l3] as Triangle;
+  });
