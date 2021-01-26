7d163a7f0c5bfd4595d59ea21aba60a3a49dd92a
Tom Cant <tomcant@gmail.com>
2021-01-26 22:28:43 +0000

    Added 2016/06

---
+++ 2016/solutions/06/index.ts
@@ -0,0 +1,23 @@
+const getMostCommonChar = (msgs: string[], idx: number): string => getCharsByCountAsc(msgs, idx).pop();
+const getLeastCommonChar = (msgs: string[], idx: number): string => getCharsByCountAsc(msgs, idx).shift();
+
+const getCharsByCountAsc = (msgs: string[], idx: number): string[] =>
+  [...getCharCountsForIndex(msgs, idx).entries()].sort(([, a], [, b]) => +a - +b).map(([char]) => char);
+
+const getCharCountsForIndex = (msgs: string[], idx: number): Map<string, number> =>
+  msgs.reduce((counts, msg) => counts.set(msg[idx], counts.get(msg[idx]) + 1 || 1), new Map());
+
+const reduceMessages = (msgs: string[], reduceCharsFn: Function): string => {
+  let msg = '';
+
+  for (let i = 0; i < msgs[0].length; ++i) {
+    msg += reduceCharsFn(msgs, i);
+  }
+
+  return msg;
+};
+
+export const part1 = (msgs: string[]): string => reduceMessages(msgs, getMostCommonChar);
+export const part2 = (msgs: string[]): string => reduceMessages(msgs, getLeastCommonChar);
+
+export const parseInput = (input: string): string[] => input.split('\n');
