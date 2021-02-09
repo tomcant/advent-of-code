7890f2e43f92dd72e95dc1729bfde8c311a2ab14
Tom Cant <tomcant@gmail.com>
2021-02-09 08:40:31 +0000

    Added 2016/12

---
+++ 2016/solutions/12/index.ts
@@ -0,0 +1,46 @@
+type Operator = 'cpy' | 'inc' | 'dec' | 'jnz';
+type Instruction = { op: Operator, args: string[] };
+type Registers = Map<string, number>;
+
+const run = (instructions: Instruction[], initialRegisters: Registers): Registers => {
+  const registers = initialRegisters;
+  let nextIdx = 0;
+
+  while (nextIdx < instructions.length) {
+    const { op, args } = instructions[nextIdx];
+    let idxIncrement = 1;
+
+    switch (op) {
+      case 'cpy':
+        registers.set(args[1], registers.has(args[0]) ? registers.get(args[0]) : +args[0]);
+        break;
+      case 'inc':
+        registers.set(args[0], registers.get(args[0]) + 1);
+        break;
+      case 'dec':
+        registers.set(args[0], registers.get(args[0]) - 1);
+        break;
+      case 'jnz':
+        if (registers.get(args[0]) || +args[0]) {
+          idxIncrement = +args[1];
+        }
+        break;
+    }
+
+    nextIdx += idxIncrement;
+  }
+
+  return registers;
+};
+
+export const part1 = (instructions: Instruction[]): number =>
+  run(instructions, new Map<string, number>([['a', 0], ['b', 0], ['c', 0], ['d', 0]])).get('a');
+
+export const part2 = (instructions: Instruction[]): number =>
+  run(instructions, new Map<string, number>([['a', 0], ['b', 0], ['c', 1], ['d', 0]])).get('a');
+
+export const parseInput = (input: string): Instruction[] =>
+  input.split('\n').map(line => {
+    const [, op, args] = line.match(/([^\s]+)\s(.+)/);
+    return { op: op as Operator, args: args.split(' ') };
+  });
