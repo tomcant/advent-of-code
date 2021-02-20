7a62a24625cd2f0b43052fbb8890fc929a0d4254
Tom Cant <tomcant@gmail.com>
2021-02-20 13:09:09 +0000

    Added 2016/16

---
+++ 2016/solutions/16/index.ts
@@ -0,0 +1,30 @@
+const genData = (initial: string, len: number): string => {
+  let data = initial;
+
+  while (data.length < len) {
+    data += '0' + data.split('').map(c => 1 - +c).reverse().join('');
+  }
+
+  return data.substr(0, len);
+};
+
+const genChecksum = (data: string): string => {
+  let checksum = data;
+
+  do {
+    let loopChecksum = '';
+
+    for (let i = 0; i < checksum.length; i += 2) {
+      loopChecksum += checksum[i] === checksum[i + 1] ? 1 : 0;
+    }
+
+    checksum = loopChecksum;
+  } while (~checksum.length & 1);
+
+  return checksum;
+};
+
+export const part1 = (input: string): string => genChecksum(genData(input, 272));
+export const part2 = (input: string): string => genChecksum(genData(input, 35651584));
+
+export const parseInput = (input: string): string => input;
