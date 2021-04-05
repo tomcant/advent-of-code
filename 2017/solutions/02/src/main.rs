95cd63f2b814e203b9f92174ad854a5aadde1ab1
Tom Cant <tomcant@gmail.com>
2021-04-05 17:09:21 +0100

    Add 2017/02

---
+++ 2017/solutions/02/src/main.rs
@@ -0,0 +1,61 @@
+fn main() {
+    let input = std::include_str!("input.txt");
+
+    println!("Part 1: {}", part1(parse_input(input)));
+    println!("Part 2: {}", part2(parse_input(input)));
+}
+
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    let mut spreadsheet: Vec<Vec<i32>> = vec![];
+
+    for line in input.lines() {
+        let mut row: Vec<i32> = vec![];
+
+        for number in line.split_whitespace() {
+            row.push(number.parse().unwrap());
+        }
+
+        spreadsheet.push(row);
+    }
+
+    return spreadsheet;
+}
+
+fn part1(spreadsheet: Vec<Vec<i32>>) -> i32 {
+    let mut checksum = 0;
+
+    for row in spreadsheet {
+        let mut min = f32::INFINITY as i32;
+        let mut max = -f32::INFINITY as i32;
+
+        for number in row {
+            if number < min {
+                min = number;
+            }
+            if number > max {
+                max = number;
+            }
+        }
+
+        checksum += max - min;
+    }
+
+    return checksum;
+}
+
+fn part2(spreadsheet: Vec<Vec<i32>>) -> i32 {
+    let mut checksum = 0;
+
+    for row in spreadsheet {
+        'row: for number in &row {
+            for other_number in row.iter().filter(|&n| n < &number) {
+                if number % other_number == 0 {
+                    checksum += number / other_number;
+                    break 'row;
+                }
+            }
+        }
+    }
+
+    return checksum;
+}
