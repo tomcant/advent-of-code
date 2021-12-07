e77b5faa4083192bc6e5ac172ad2e7f4ccb9c88c
Tom Cant <tomcant@gmail.com>
2021-12-07 08:45:30 +0000

    Add 2021/fsharp/07

---
+++ 2021/fsharp/solutions/07/Solution.fsx
@@ -0,0 +1,14 @@
+open Helpers
+
+let parseInput = splitOn "," >> List.map int
+
+let findCheapest costFn crabs =
+  crabs
+  |> List.map
+       (fun pos ->
+         crabs
+         |> List.sumBy (fun p -> costFn (abs (pos - p))))
+  |> List.min
+
+let part1 = findCheapest id
+let part2 = findCheapest (fun n -> n * (n + 1) / 2)
