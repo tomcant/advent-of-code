44ff0df273ba9fc7683c65f6f4c034cb110d6bdc
Tom Cant <tomcant@gmail.com>
2022-11-06 17:39:40 +0000

    Add 2018/python/05

---
+++ 2018/python/solutions/05/solution.py
@@ -0,0 +1,33 @@
+import string
+
+
+def part1(polymer):
+    return len(reduce(polymer))
+
+
+def part2(polymer):
+    replacements = [{ord(char): '', ord(char.upper()): ''}
+                    for char in string.ascii_lowercase]
+
+    return min([len(reduce(polymer.translate(reps))) for reps in replacements])
+
+
+def reduce(polymer):
+    while True:
+        next = react(polymer)
+        if next == polymer:
+            return next
+        polymer = next
+
+
+def react(polymer):
+    def react_pairs(pairs):
+        return ''.join(filter(lambda p: len(p) < 2 or not is_match(p), pairs))
+
+    next = react_pairs([polymer[i:i+2] for i in range(0, len(polymer), 2)])
+    return next[0] + react_pairs([next[i:i+2] for i in range(1, len(next), 2)])
+
+
+def is_match(pair):
+    return (pair[0].islower() and pair[1].isupper() and pair[0] == pair[1].lower()) \
+        or (pair[0].isupper() and pair[1].islower() and pair[0] == pair[1].upper())
