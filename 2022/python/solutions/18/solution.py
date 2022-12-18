0f97fb68993e3ddb211cf84d931312c59aa87e39
Tom Cant <tomcant@gmail.com>
2022-12-18 20:49:05 +0000

    Add 2022/python/18

---
+++ 2022/python/solutions/18/solution.py
@@ -0,0 +1,51 @@
+from collections import deque
+from functools import cache
+
+
+def parse_input(input):
+    return set(map(lambda line: tuple(map(int, line.split(','))), input.splitlines()))
+
+
+def part1(cubes):
+    return sum(pos not in cubes for cube in cubes for pos in get_neighbours(cube))
+
+
+def part2(cubes):
+    min, *_, max = sorted(cubes)
+
+    outside = [
+        (min[0] - 1, min[1] - 1, min[2] - 1),
+        (max[0] + 1, max[1] + 1, max[2] + 1),
+    ]
+
+    @cache
+    def is_outside(pos):
+        queue = deque([pos])
+        seen = set()
+
+        while queue:
+            pos = queue.popleft()
+            if pos in seen or pos in cubes:
+                continue
+            seen.add(pos)
+            if pos in outside:
+                return True
+            queue += get_neighbours(pos)
+
+        return False
+
+    return sum(is_outside(pos) for cube in cubes for pos in get_neighbours(cube))
+
+
+def get_neighbours(pos):
+    return [(pos[0] + dx, pos[1] + dy, pos[2] + dz) for dx, dy, dz in DIRS]
+
+
+DIRS = [
+    (1, 0, 0),
+    (-1, 0, 0),
+    (0, 1, 0),
+    (0, -1, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+]
